f99f616878f2883c15e5236ffa0caa12a83096ac
最萌小汐 <sunyijiusyj@126.com>
2016-11-07 16:05:46 +0800

    表达式

--- src/parser.lua
+++ src/parser.lua
@@ -1,38 +1,56 @@
-local str_line_count = 0
 local line_count = 1
 
-local sp   = S' \t'^1
+local sp   = S' \t'^0
+local sps  = S' \t'^1
 local nl1  = P'\r\n' + S'\r\n'
-local nl   = nl1 / function() line_count = line_count + 1 end
-local com  = P'//' * (1-nl)^0
+local com  = P'//' * (1-nl1)^0
+local nl   = com^0 * nl1 / function() line_count = line_count + 1 end
 local quo  = P'"'
 local esc  = P'\\'
-local op1  = S'+-*/=<>' + (S'><=!' * P'=')
+local op1  = S'+-*/'
 local op2  = P'-'
-local op   = op1 + op2
-local int1 = P'0' + R'19' * R'09'^0
+local op3  = S'><=!' * P'=' + S'=><'
+local int1 = P'-'^-1 * (P'0' + R'19' * R'09'^0)
 local int2 = P'$' * R('af', 'AF', '09')^1
 local int3 = P'0' * S'xX' * R('af', 'AF', '09')^1
 local int  = int1 + int2 + int3
 local real = R'09'^0 * P'.' * R'09'^0
 local bool = P'true' + P'false'
 local str1 = esc * P(1) + (1-quo)
-local str2 = str1 + nl1 / function () str_line_count = str_line_count + 1 end
-local str  = (quo * str2^0 * quo) / function() line_count = line_count + str_line_count ; str_line_count = 0 end
+local str2 = str1 + nl
+local str  = quo * str2^0 * quo
 local id   = R('az', 'AZ') * R('az', 'AZ', '09', '__')^0
 
-local exp1 = sp^0 * (int + real + bool + str + id) * sp^0
-local exp2 = sp^0 * P'(' * exp1 * P')' * sp^0 + exp1
---local exp3 = sp^0 * op2 * exp2 + exp2 * op1 * exp2 + exp2
---local exp4 = exp3 * P'(' * exp3 * P')' + exp3 * P'[' * exp3 * ']' + exp3
-local exp  = exp2
+local exp1 = sp * (int + real + bool + str + id) * sp
+
+local exp = P{
+    'exp',
+    exp   = V'bra' + V'op3' + V'op2' + V'op1' + V'call' + V'index' + exp1,
+    exp1  = V'bra' + V'op2' + V'call' + V'index' + exp1,
+    exp2  = V'bra' + V'op2' + V'op1' + V'call' + V'index' + exp1,
+    bra   = sp * '(' * V'exp' * ')' * sp,
+    op1   = V'exp1' * (op1 * V'exp1')^0,
+    op2   = sp * op2 * V'exp',
+    op3   = V'exp2' * op3 * V'exp2',
+    args  = V'exp' * (',' * sp * V'exp')^0 + sp,
+    call  = exp1 * '(' * V'args' * ')' * sp,
+    index = exp1 * '[' * V'exp' * ']' * sp,
+}
+
+local func = P{
+    'func',
+    func = 'function' * sps * id * sps * 'takes' * sps * V'args' * sps * 'returns' * sps * id * sp * nl * V'content' * sp * 'endfunction',
+    args = id * sps * id * (sp * ',' * sp * id * sps * id)^0,
+    content = (sp * exp * sp * nl)^0
+}
 
 local err  = P(1) / function(c) error(('line[%d]: 错误的字符: "%s"'):format(line_count, c)) end
 
 return function (jass)
-    local anyword = sp + nl + com + C(exp + op + int + real + bool + str + id) + err
-    local words = Ct(anyword ^ 1):match(jass)
+    local anyword = sps + nl + C(exp, func) + err
+    local words = Ct(anyword^1):match(jass)
     print(line_count, words[#words])
+    print('用时', os.clock())
 end
 
 --[[
