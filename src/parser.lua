bbd7b26ad5921bcea1ac361726ef4789a4062bea
最萌小汐 <sunyijiusyj@126.com>
2016-11-10 20:11:45 +0800

    封装一下error

--- src/parser.lua
+++ src/parser.lua
@@ -37,11 +37,11 @@ local id   = R('az', 'AZ') * R('az', 'AZ', '09', '__')^0
 local nid  = #(1-id)
 
 local function err(str)
-    return (1-nl)^0 / function(c) error(('line[%d]: %s:\n===========================\n%s\n==========================='):format(line_count, str, c)) end
+    return ((1-nl)^1 + P(1)) / function(c) error(('line[%d]: %s:\n===========================\n%s\n==========================='):format(line_count, str, c)) end
 end
 
-local function errs(str)
-    return (1-nl)^1 / function(c) error(('line[%d]: %s:\n===========================\n%s\n==========================='):format(line_count, str, c)) end
+local function expect(p, str)
+    return p + err(str)
 end
 
 local word = sp * (real + int + bool + str + id) * sp
@@ -50,8 +50,8 @@ local exp = P{
     'exp',
     -- 由低优先级向高优先级递归
     exp   = V'op7' + V'exp7',
-    exp1  = (V'bra' + V'func' + V'call' + V'index' + word) * com^-1 + errs'表达式不正确',
-    exp2  = V'op1' + V'exp1',
+    exp1  = (V'bra' + V'func' + V'call' + V'index' + word) * com^-1,
+    exp2  = V'op1' + expect(V'exp1', '表达式不正确'),
     exp3  = V'op2' + V'exp2',
     exp4  = V'op3' + V'exp3',
     exp5  = V'op4' + V'exp4',
@@ -59,59 +59,61 @@ local exp = P{
     exp7  = V'op6' + V'exp6',
 
     -- 由于消耗了字符串,可以递归回顶层
-    op1   = sp * op1 * (V'exp1' + err'符号"-"错误'),
-    op2   = sp * op2 * (V'exp1' + err'符号"not"错误'),
+    op1   = sp * op1 * expect(V'exp1', '符号"-"错误'),
+    op2   = sp * op2 * expect(V'exp1', '符号"not"错误'),
 
     -- 由于不消耗字符串,只允许向下递归
-    op3   = V'exp3' * (op3 * (V'exp3' + err'符号"*/"错误'))^0,
-    op4   = V'exp4' * (op4 * (V'exp4' + err'符号"+-"错误'))^0,
-    op5   = V'exp5' * op5 * (V'exp5' + err'逻辑判断符错误'),
-    op6   = V'exp6' * (op6 * (V'exp6' + err'符号"and"错误'))^0,
-    op7   = V'exp7' * (op7 * (V'exp7' + err'符号"or"错误'))^0,
+    op3   = V'exp3' * (op3 * expect(V'exp3', '符号"*/"错误'))^0,
+    op4   = V'exp4' * (op4 * expect(V'exp4', '符号"+-"错误'))^0,
+    op5   = V'exp5' * op5 * expect(V'exp5', '逻辑判断符错误'),
+    op6   = V'exp6' * (op6 * expect(V'exp6', '符号"and"错误'))^0,
+    op7   = V'exp7' * (op7 * expect(V'exp7', '符号"or"错误'))^0,
 
     -- 由于消耗了字符串,可以递归回顶层
-    bra   = sp * '(' * (V'exp' * ')' * sp + err'括号不匹配'),
-    call  = word * '(' * (V'args' * ')' * sp + err'函数调用不正确'),
-    args  = #(sp * ')') + V'exp' * V'narg' + err'参数不正确',
-    narg  = ',' * (V'exp' * V'narg' + err'参数不正确') + sp,
-    index = word * '[' * (V'exp' * ']' * sp + err'获取变量数组不正确'),
+    bra   = sp * '(' * expect(V'exp' * ')' * sp, '括号不匹配'),
+    call  = word * '(' * expect(V'call2', '函数调用不正确'),
+    call2 = expect(V'args', '参数不正确') * ')' * sp,
+    args  = #(sp * ')') + V'exp' * V'narg',
+    narg  = ',' * expect(V'exp' * V'narg', '参数不正确') + sp,
+    index = word * '[' * expect(V'exp' * ']' * sp, '获取变量数组不正确'),
     func  = sp * 'function' * sps * id * sp,
 }
 
 local typedef = P{
     'def',
-    def  = sp * 'type' * (sps * id + err'变量类型定义错误') * V'ext',
-    ext  = sps * 'extends' * sps * id + err'变量类型继承错误',
+    def  = sp * 'type' * expect(sps * id, '变量类型定义错误') * expect(V'ext', '变量类型继承错误'),
+    ext  = sps * 'extends' * sps * id,
 }
 
 local global = P{
     'global',
-    global = sp * 'globals' * (V'nval' * V'gend' + err'全局变量未知错误'),
-    nval   = spl * V'val'^0 + err'全局变量未知声明错误',
+    global = sp * 'globals' * expect(V'gdef', '全局变量未知错误'),
+    gdef   = expect(V'nval', '全局变量未知声明错误') * expect(V'gend', '缺少endglobals'),
+    nval   = spl * V'val'^0,
     val    = spl + (V'const' + V'array' + V'set' + V'def') * spl,
     def    = sp * id * sps * id,
-    set    = sp * V'def' * sp * '=' * (exp + err'全局变量声明时赋值错误'),
-    array  = sp * id * sps * 'array' * (sps * id + err'全局变量数组声明错误'),
-    const  = sp * 'constant' * (sps * V'set' + err'全局常量声明错误'),
-    gend   = sp * 'endglobals' + err'全局变量结束符错误',
+    set    = sp * V'def' * sp * '=' * expect(exp, '全局变量声明时赋值错误'),
+    array  = sp * id * sps * 'array' * expect(sps * id, '全局变量数组声明错误'),
+    const  = sp * 'constant' * expect(sps * V'set', '全局常量声明错误'),
+    gend   = sp * 'endglobals',
 }
 
 local loc = P{
     'loc',
-    loc = sp * 'local' * (sps * V'val' + err'局部变量声明错误'),
+    loc = sp * 'local' * expect(sps * V'val', '局部变量声明错误'),
     val    = V'array' + V'set' + V'def',
     def    = id * sps * id,
     set    = id * sps * id * sp * '=' * exp,
-    array  = id * sps * 'array' * (sps * id + err'局部变量数组声明错误'),
+    array  = id * sps * 'array' * expect(sps * id, '局部变量数组声明错误'),
 }
 
 local line = P{
     'line',
-    line  = sp * ('call' * V'call' + 'set' * V'set' + 'return' * V'rtn'),
-    call  = sps * exp + err'call语法不正确',
-    set   = sps * V'val' * '=' * exp + err'set语法不正确',
-    rtn   = sp * #(1-nl) * (exp + err'return语法不正确') + P(true),
-    val   = sp * id * sp * '[' * exp * ']' * sp + sp * id * sp + err'变量不正确',
+    line  = sp * ('call' * expect(V'call', 'call语法不正确') + 'set' * expect(V'set', 'set语法不正确') + 'return' * V'rtn'),
+    call  = sps * exp,
+    set   = sps * expect(V'val', '变量不正确') * '=' * exp,
+    rtn   = sp * #(1-nl) * expect(exp, 'return语法不正确') + P(true),
+    val   = sp * id * sp * '[' * exp * ']' * sp + sp * id * sp,
 }
 
 local in_loop_count = 0
@@ -119,39 +121,40 @@ local logic = P{
     'logic',
     logic    = V'lif' + V'lloop',
 
-    lif      = sp * 'if' * (nid * exp * V'ithen' * V'icontent' + err'if语句未知错误'),
-    ithen    = sp * 'then' * spl + err'if后面没有then',
-    icontent =  V'iendif' + (spl + V'logic' + V'ielseif' + V'ielse' + V'lexit' + line) * V'icontent' + err'if内容错误',
-    ielseif  = sp * 'elseif' * (nid * exp * V'ithen' + err'elseif错误'),
+    lif      = sp * 'if' * expect(V'ihead', 'if语句未知错误'),
+    ihead    = nid * exp * expect(V'ithen', 'if后面没有then') * expect(V'icontent', 'if内容错误'),
+    ithen    = sp * 'then' * spl,
+    icontent =  V'iendif' + (spl + V'logic' + V'ielseif' + V'ielse' + V'lexit' + line) * V'icontent',
+    ielseif  = sp * 'elseif' * expect(nid * exp * V'ithen', 'elseif错误'),
     ielse    = sp * 'else' * spl,
     iendif   = sp * 'endif' * spl,
 
-    lloop    = V'lhead' * (V'lcontent' + err'loop语句未知错误'),
+    lloop    = V'lhead' * expect(V'lcontent', 'loop语句未知错误'),
     lhead    = sp * 'loop' * spl / function() in_loop_count = in_loop_count + 1 end,
-    lcontent = V'lendloop' + (spl + V'logic' + V'lexit' + line) * V'lcontent' + err'loop内容错误',
-    lexit    = sp * 'exitwhen' * (sps * exp + err'exitwhen错误') / function(c) if in_loop_count <= 0 then err'exitwhen必须在loop内部':match(c) end end,
+    lcontent = V'lendloop' + (spl + V'logic' + V'lexit' + line) * V'lcontent',
+    lexit    = sp * 'exitwhen' * expect(sps * exp, 'exitwhen错误') / function(c) if in_loop_count <= 0 then err'exitwhen必须在loop内部':match(c) end end,
     lendloop = sp * 'endloop' / function() in_loop_count = in_loop_count - 1 end,
 }
 
 local func = P{
     'fct',
     fct      = V'native' + V'func',
-    native   = sp * 'native' * (V'fhead' + err'native函数未知错误'),
-    func     = sp * 'function' * (V'fhead' * V'fcontent' * V'fend' + err'自定义函数未知错误'),
-    fhead    = sps * V'fname' * V'fargs' * V'freturns' + err'函数声明格式不正确',
-    fname    = sp * id * sp + err'函数名称不正确',
-    fargs    = sp * 'takes' * sps * (V'anull' + V'anarg') + err'函数的参数声明不正确',
+    native   = sp * 'native' * expect(V'fhead', 'native函数未知错误'),
+    func     = sp * 'function' * expect(V'fhead', '函数声明格式不正确') * expect(V'fcontent', '函数主体不正确') * expect(V'fend', '缺少endfunction'),
+    fhead    = sps * expect(V'fname', '函数名称不正确') * expect(V'fargs', '函数的参数声明不正确') * expect(V'freturns', '函数的返回格式不正确'),
+    fname    = sp * id * sp,
+    fargs    = sp * 'takes' * sps * (V'anull' + V'anarg'),
     arg      = sp * id * sps * id * sp,
     anull    = sp * 'nothing' * sp,
     anarg    = sp * V'arg' * (',' * V'arg')^0,
-    freturns = sp * 'returns' * sps * id * spl + err'函数的返回格式不正确',
-    fcontent = sp * V'flocal' * V'flines' + err'函数主体不正确',
-    flocal   = (spl + loc)^0 + err'函数局部变量区域不正确',
-    flines   = (spl + logic + line)^0 + err'函数代码区域不正确',
-    fend     = sp * 'endfunction' + err'函数结束符不正确',
+    freturns = sp * 'returns' * sps * id * spl,
+    fcontent = sp * expect(V'flocal', '函数局部变量区域不正确') * expect(V'flines', '函数代码区域不正确'),
+    flocal   = (spl + loc)^0,
+    flines   = (spl + logic + line)^0,
+    fend     = sp * 'endfunction',
 }
 
-local pjass = (ign + typedef + global + func + errs'语法不正确')^0
+local pjass = (ign + typedef + global + func + err'语法不正确')^0
 
 local mt = {}
 setmetatable(mt, mt)
