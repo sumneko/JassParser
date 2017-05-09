6acca2d4030b94f296f075d87cfd09ba1804f522
最萌小汐 <sunyijiusyj@126.com>
2017-05-09 13:55:59 +0800

    校验脚本

--- src/main.lua
+++ src/main.lua
@@ -7,6 +7,7 @@ end)()
 require 'filesystem'
 require 'utility'
 local uni  = require 'unicode'
+local parser = require 'parser'
 
 local function load_in_env(name, env)
     local path = package.searchpath(name, package.path)
@@ -14,7 +15,25 @@ local function load_in_env(name, env)
 end
 
 local function main()
-    require 'test'
+    if arg[1] then
+        local exepath  = package.cpath:sub(1, package.cpath:find(';')-6)
+        local root     = fs.path(uni.a2u(exepath)):parent_path():parent_path():parent_path()
+        local common   = io.load(root / 'src' / 'jass' / 'common.j')
+        local blizzard = io.load(root / 'src' / 'jass' / 'blizzard.j')
+
+        local path = fs.path(arg[1])
+        local jass = io.load(path)
+
+        local ast, grms
+        local suc, e = xpcall(function()
+            ast, grms = parser(common,   'common.j',   ast)
+            ast, grms = parser(blizzard, 'blizzard.j', ast)
+            ast, grms = parser(jass,     'war3map.j',  ast)
+        end, error_handle)
+        print('脚本校验完成', #jass)
+    else
+        require 'test'
+    end
     print('完成')
 end
 
