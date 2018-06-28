2ed3ebfc940c79499731f8badcba8890864647f7
最萌小汐 <sumneko@hotmail.com>
2018-06-28 15:25:28 +0800

    暂存

--- src/parser/grammarlabel.lua
+++ src/parser/grammarlabel.lua
@@ -11,7 +11,10 @@ end
 grammar 'Common' [[
 Comment     <- '//' (!%nl)*
 Sp          <- (%s / %t / '\xEF\xBB\xBF' / Comment)*
+Nl          <- Sp %nl+
 Cut         <- ![a-zA-Z0-9_]+
+COMMA       <- Sp ','
+ASSIGN      <- Sp '=' !'='
 GLOBALS     <- Sp 'globals' Cut
 ENDGLOBALS  <- Sp 'endglobals' Cut
 CONSTANT    <- Sp 'constant' Cut
@@ -37,37 +40,76 @@ ELSE        <- Sp 'else' Cut
 LOOP        <- Sp 'loop' Cut
 ENDLOOP     <- Sp 'endloop' Cut
 EXITWHEN    <- Sp 'exitwhen' Cut
-RESERVED   <- GLOBALS / ENDGLOBALS / CONSTANT / NATIVE / ARRAY / AND / OR / NOT / TYPE / EXTENDS / FUNCTION / ENDFUNCTION / NOTHING / TAKES / RETURNS / CALL / SET / RETURN / IF / ENDIF / ELSEIF / ELSE / LOOP / ENDLOOP / EXITWHEN)
+LOCAL       <- Sp 'local' Cut
+RESERVED    <- GLOBALS / ENDGLOBALS / CONSTANT / NATIVE / ARRAY / AND / OR / NOT / TYPE / EXTENDS / FUNCTION / ENDFUNCTION / NOTHING / TAKES / RETURNS / CALL / SET / RETURN / IF / ENDIF / ELSEIF / ELSE / LOOP / ENDLOOP / EXITWHEN
 ]]
 
 grammar 'Word' [[
-Id          <- !RESERVED Sp ([a-zA-Z]) ([a-zA-Z0-9_])*
+Name        <- !RESERVED Sp ([a-zA-Z]) ([a-zA-Z0-9_])*
 NULL        <- Sp 'null' Cut
 Boolean     <- Sp ('true' / 'false') Cut
 String      <- Sp '"' ('\\\\' / '\\"' / !'"')* '"'
-Real        <- Sp "-"? Sp ("." [0-9]+) / [0-9]+ "." [0-9]*)
+Real        <- Sp '-'? Sp ('.' [0-9]+) / [0-9]+ '.' [0-9]*)
 Integer     <- Integer16 / Integer10 / Integer256
-Integer10   <- Sp "-"? Sp ("0" / ([1-9] [0-9]*))
-Integer16   <- Sp "-"? Sp ("$" / "0x" / "0X") ([a-fA-F0-9])+
-Integer256  <- Sp "-"? Sp "'" ("\\\\" / "\\'" / !"'")* "'"
-Word        <- NULL / Boolean / String / Real / Integer / Id
+Integer10   <- Sp '-'? Sp ('0' / ([1-9] [0-9]*))
+Integer16   <- Sp '-'? Sp ('$' / '0x' / '0X') ([a-fA-F0-9])+
+Integer256  <- Sp '-'? Sp "'" ('\\\\' / "\\'" / !"'")* "'"
+Word        <- NULL / Boolean / String / Real / Integer / Name
 ]]
 
 grammar 'Compare' [[
-GT          <- Sp ">"
-GE          <- Sp ">="
-LT          <- Sp "<"
-LE          <- Sp "<="
-EQ          <- Sp "=="
-UE          <- Sp "!="
+GT          <- Sp '>'
+GE          <- Sp '>='
+LT          <- Sp '<'
+LE          <- Sp '<='
+EQ          <- Sp '=='
+UE          <- Sp '!='
 Compare     <- UE / EQ / LE / LT / GE / GT
 ]]
 
+grammar 'Operator' [[
+ADD         <- Sp '+'
+SUB         <- Sp '-'
+MUL         <- Sp '*'
+DIV         <- Sp '/'
+NEG         <- Sp '-'
+]]
+
+grammar 'Paren' [[
+PL          <- Sp '('
+PR          <- Sp ')'
+BL          <- Sp '['
+BR          <- Sp ']'
+]]
+
 grammar 'Exp' [[
-ExpAnd      <- Sp ExpOr      (AND     ExpOr)*
-ExpOr       <- Sp ExpCompare (OR      ExpCompare)*
-ExpCompare  <- Sp ExpNot     (Compare ExpNot)*
-ExpNot      <- 
+ExpDef      <- Sp ExpAnd
+ExpAnd      <- Sp ExpOr      (AND         ExpOr)*
+ExpOr       <- Sp ExpCompare (OR          ExpCompare)*
+ExpCompare  <- Sp ExpNot     (Compare     ExpNot)*
+ExpNot      <- Sp             NOT*        ExpAdd
+ExpAdd      <- Sp ExpMul     ((ADD / SUB) ExpMul)*
+ExpMul      <- Sp Exp        ((MUL / DIV) Exp)*
+
+Exp         <- Sp (ExpParen / ExpCode / ExpCall / ExpValue / ExpNeg)
+ExpParen    <- Sp PL ExpDef PR
+ExpCode     <- Sp FUNCTION Name
+ExpCall     <- Sp Name PL ExpDef (COMMA ExpDef)* PR
+ExpValue    <- Sp Word (BL ExpDef BR)?
+ExpNeg      <- Sp NEG Exp
+]]
+
+grammar 'Type' [[
+Type        <- Sp TYPE Name EXTENDS Name
+]]
+
+grammar 'Globals' [[
+Globals     <- Sp GLOBALS Nl (Global? Nl)* ENDGLOBALS
+Global      <- Sp CONSTANT? Name ARRAY? Name (ASSIGN Exp)?
+]]
+
+grammar 'Local' [[
+Local       <- Sp LOCAL 
 ]]
 
 local Pjass = re.compile(scriptBuf, terror)
