c79790dc48ee9187130b2d861364315f9449ebda
最萌小汐 <sumneko@hotmail.com>
2018-06-28 22:02:30 +0800

    暂存

--- src/parser/grammarlabel.lua
+++ src/parser/grammarlabel.lua
@@ -1,17 +1,17 @@
-local re = require 'relabel'
+local re = require 'parser.relabel'
 local lpeg = require 'lpeglabel'
 
 local scriptBuf = ''
 local function grammar(tag)
     return function (script)
-        scriptBuf = scriptBuf .. script
+        scriptBuf = script .. '\r\n' .. scriptBuf
     end
 end
 
 grammar 'Common' [[
 Comment     <- '//' (!%nl)*
 Sp          <- (%s / %t / '\xEF\xBB\xBF' / Comment)*
-Nl          <- Sp %nl+
+Nl          <- (Sp %nl)+
 Cut         <- ![a-zA-Z0-9_]+
 COMMA       <- Sp ','
 ASSIGN      <- Sp '=' !'='
@@ -49,7 +49,7 @@ Name        <- !RESERVED Sp ([a-zA-Z]) ([a-zA-Z0-9_])*
 NULL        <- Sp 'null' Cut
 Boolean     <- Sp ('true' / 'false') Cut
 String      <- Sp '"' ('\\\\' / '\\"' / !'"')* '"'
-Real        <- Sp '-'? Sp ('.' [0-9]+) / [0-9]+ '.' [0-9]*)
+Real        <- Sp '-'? Sp ('.' [0-9]+) / ([0-9]+ '.' [0-9]*)
 Integer     <- Integer16 / Integer10 / Integer256
 Integer10   <- Sp '-'? Sp ('0' / ([1-9] [0-9]*))
 Integer16   <- Sp '-'? Sp ('$' / '0x' / '0X') ([a-fA-F0-9])+
@@ -83,49 +83,146 @@ BR          <- Sp ']'
 ]]
 
 grammar 'Exp' [[
-ExpDef      <- Sp ExpAnd
-ExpAnd      <- Sp ExpOr      (AND         ExpOr)*
-ExpOr       <- Sp ExpCompare (OR          ExpCompare)*
-ExpCompare  <- Sp ExpNot     (Compare     ExpNot)*
-ExpNot      <- Sp             NOT*        ExpAdd
-ExpAdd      <- Sp ExpMul     ((ADD / SUB) ExpMul)*
-ExpMul      <- Sp Exp        ((MUL / DIV) Exp)*
+Exp         <- EAnd
+EAnd        <- EOr      (AND         EOr)*
+EOr         <- ECompare (OR          ECompare)*
+ECompare    <- ENot     (Compare     ENot)*
+ENot        <-           NOT*        EAdd
+EAdd        <- EMul     ((ADD / SUB) EMul)*
+EMul        <- EUnit    ((MUL / DIV) EUnit)*
+EUnit       <- EParen / ECode / ECall / EValue / ENeg
 
-Exp         <- Sp (ExpParen / ExpCode / ExpCall / ExpValue / ExpNeg)
-ExpParen    <- Sp PL ExpDef PR
-ExpCode     <- Sp FUNCTION Name
-ExpCall     <- Sp Name PL ExpDef (COMMA ExpDef)* PR
-ExpValue    <- Sp Word (BL ExpDef BR)?
-ExpNeg      <- Sp NEG Exp
+EParen      <- PL Exp PR
+
+ECode       <- FUNCTION ECodeFunc
+ECodeFunc   <- Name
+
+ECall       <- ECallFunc PL ECallArgs PR
+ECallFunc   <- Name
+ECallArgs   <- ECallArg (COMMA ECallArg)*
+ECallArg    <- Exp
+
+EValue      <- EVari / EVar / EWord
+EVari       <- EVar BL EIndex BR
+EIndex      <- Exp
+EVar        <- Name
+EWord       <- Word
+
+ENeg        <- NEG EUnit
 ]]
 
 grammar 'Type' [[
-Type        <- Sp TYPE Name EXTENDS Name
+Type        <- TYPE TChild EXTENDS TParent
+TChild      <- Name
+TParent     <- Name
 ]]
 
 grammar 'Globals' [[
-Globals     <- Sp GLOBALS Nl (Global? Nl)* ENDGLOBALS
-Global      <- Sp CONSTANT? Name ARRAY? Name (ASSIGN Exp)?
+Globals     <- GLOBALS Global* GEnd
+Global      <- Nl GConstant? GType GArray? GName GExp?
+GConstant   <- CONSTANT
+GType       <- Name
+GArray      <- ARRAY
+GName       <- Name
+GExp        <- ASSIGN Exp
+GEnd        <- Nl ENDGLOBALS
 ]]
 
-grammar 'Local' [[
-Local       <- Sp LOCAL 
+grammar 'Locals' [[
+Locals      <- Local*
+Local       <- Nl LOCAL LType LArray? LName LExp?
+LType       <- Name
+LArray      <- ARRAY
+LName       <- Name
+LExp        <- ASSIGN Exp
 ]]
 
-local Pjass = re.compile(scriptBuf, terror)
+grammar 'Actions' [[
+Actions     <- Action*
+Action      <- Nl (ACall / ASet / ASeti / AReturn / AExit / ALogic / ALoop)
+
+ACall       <- CALL ACallFunc PL ACallArgs PR
+ACallFunc   <- Name
+ACallArgs   <- ACallArg (COMMA ECallArg)*
+ACallArg    <- Exp
+
+ASet        <- SET ASetName ASSIGN ASetValue
+ASetName    <- Name
+ASetValue   <- Exp
+
+ASeti       <- SET ASetiName BL ASetiIndex BR ASSIGN ASetiValue
+ASetiName   <- Name
+ASetiIndex  <- Exp
+ASetiValue  <- Exp
+
+AReturn     <- RETURN AReturnExp?
+AReturnExp  <- Exp
+
+AExit       <- EXITWHEN AExitExp
+AExitExp    <- Exp
+
+ALogic      <- LIf LElseif* LElse? LEnd
+LIf         <- IF     Exp THEN Actions
+LElseif     <- ELSEIF EXP THEN Actions
+LElse       <- ELSE            Actions
+LEnd        <- ENDIF
+]]
+
+grammar 'Native' [[
+Native      <- NConstant? NATIVE NName NTakes NReturns
+NConstant   <- CONSTANT
+NName       <- Name
+NTakes      <- TAKES (NTNothing / NArgs)
+NTNothing   <- NOTHING
+NArgs       <- NArg (COMMA NArg)*
+NArg        <- NArgType NArgName
+NArgType    <- Name
+NArgName    <- Name
+NReturns    <- RETURNS (NRNothing / NRExp)
+NRNothing   <- NOTHING
+NRExp       <- Exp
+]]
+
+grammar 'Functions' [[
+Functions   <- Function (Nl Function)*
+Function    <- FConstant? FUNCTION FName FTakes FReturns FLocals FActions FEnd
+FConstant   <- CONSTANT
+FName       <- Name
+FTakes      <- TAKES (FTNothing / FArgs)
+FTNothing   <- NOTHING
+FArgs       <- FArg (COMMA FArg)*
+FArg        <- FArgType FArgName
+FArgType    <- Name
+FArgName    <- Name
+FReturns    <- RETURNS (FRNothing / FRExp)
+FRNothing   <- NOTHING
+FRExp       <- Exp
+FLocals     <- Locals
+FActions    <- Actions
+FEnd        <- Nl ENDFUNCTION
+]]
+
+grammar 'Jass' [[
+Jass        <- Chunks (!. / %{eof})
+Chunks      <- Nl? Chunk (Nl Chunk)* Nl?
+Chunk       <- Type / Globals / Native / Function
+]]
+
+local Pjass = re.compile(scriptBuf)
 
 local mt = {}
 setmetatable(mt, mt)
 
 function mt:__call(jass, file, mode)
+    local comments = {}
     local r, e, pos = Pjass:match(jass)
     if not r then
         local line, col = re.calcline(s, pos)
         local msg = "Error at line " .. line .. " (col " .. col .. "): "
-        return r, msg
+        error(msg .. e)
     end
 
-    return r
+    return r, comments
 end
 
 return mt
