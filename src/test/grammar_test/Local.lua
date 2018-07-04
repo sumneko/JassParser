7574e13ed2e68520229dc3e92460703697053d69
最萌小汐 <sumneko@hotmail.com>
2018-07-04 13:57:04 +0800

    Local 测试

--- src/test/grammar_test/Local.lua
+++ src/test/grammar_test/Local.lua
@@ -1,7 +1,28 @@
-test 'local unit u'
+check 'local unit u'
 {
     type = 'unit',
     name = 'u',
+    file = 'war3map.j',
+    line = 1,
+}
+
+check 'local integer a = 1'
+{
+    type = 'integer',
+    name = 'a',
+    file = 'war3map.j',
+    line = 1,
+    [1]  = {
+        type = 'integer',
+        value = 1,
+    }
+}
+
+check 'local integer array a'
+{
+    type = 'integer',
+    name = 'a',
+    array = true,
     file = 'war3map.j',
     line = 1,
 }
