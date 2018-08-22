6825c7b3d04b7b5ea5d87307eebf723fd07dfa6f
最萌小汐 <sumneko@hotmail.com>
2018-08-22 10:38:20 +0800

    更新测试

---
+++ test/should-fail/缺少endif-2.j
@@ -0,0 +1,10 @@
+function f takes nothing returns nothing
+    local integer x
+    local integer y
+    if true then
+        set x = 4
+        loop
+        exitwhen true
+      set y = 3
+    set y = 8
+endfunction
---
+++ test/should-fail/缺少endif-2.lua
@@ -0,0 +1,21 @@
+local errors = ...
+
+local expect = {
+    ['缺少endif。'] = 10,
+    ['缺少endloop。'] = 10,
+}
+
+for _, error in ipairs(errors) do
+    local line, err = error.line, error.err
+    if expect[err] ~= line then
+        return false, ('第[%d]行出现错误：%s'):format(line, err)
+    end
+    expect[err] = nil
+end
+
+local err, line = next(expect)
+if line then
+    return false, ('没有检查出[%s]'):format(err)
+end
+
+return true
